ddc2ba048823163d71de2690313dcaea1c913623
José Rodríguez <2300456r@student.gla.ac.uk>
2024-11-06 13:22:12 +0000

    Change bruteForce.py so that it can be run from another Python file. Its main function returns the data of interest

--- bruteForce.py
+++ bruteForce.py
@@ -1,5 +1,3 @@
-import gurobipy as gp
-from gurobipy import GRB
 import sys
 import numpy as np
 import time
@@ -16,7 +14,7 @@ def readFile(filename):
                 inputArray[i][j] = int(values[j])
         return inputArray
 
-def getMaxMinValuations(valuations):
+def getMaxMinValuations(valuations, n):
     maxValuation = valuations[0][0]
     minValuation = valuations[0][0]
     for i in range(n):
@@ -27,7 +25,7 @@ def getMaxMinValuations(valuations):
                 minValuation = valuations[i][j]
     return minValuation,maxValuation
 
-def getMaxDegree(seatGraph):
+def getMaxDegree(seatGraph, n):
     maxDegree = 0
     for i in range(n):
         degree = 0
@@ -37,7 +35,7 @@ def getMaxDegree(seatGraph):
             maxDegree = degree
     return maxDegree
 
-def getNeighbours(u):
+def getNeighbours(u, seatGraph):
     neighbours = np.zeros(int(sum(seatGraph[u])))
     i = 0
     for v in range(n):
@@ -46,50 +44,54 @@ def getNeighbours(u):
             i += 1
     return neighbours
 
-valFile = sys.argv[1]
-seatFile = sys.argv[2]
-valuations = readFile(valFile)
-seatGraph = readFile(seatFile)
-
-n = len(valuations)
-minValuation, maxValuation = getMaxMinValuations(valuations)
-absMaxValuation = max(abs(minValuation), abs(maxValuation))
-maxDegree = getMaxDegree(seatGraph)
-minUtility = minValuation*maxDegree
-maxUtility = maxValuation*maxDegree
-absMaxutility = max(abs(minUtility), abs(maxUtility))
-epsilon = 0.1
-
-start_total_time = time.time()
-
-remaining = []
-for i in range(n):
-    remaining.append(i)
-
-def calculateUtility(permutation):
+def calculateUtility(permutation, valuations, seatGraph, n):
     utility  = 0
     for p in range(n):
         for q in range(n):
             utility += valuations[p][q]*seatGraph[permutation[p]][permutation[q]]
     return utility
 
-def createPermutation(permutation, remaining, currentTotalUtility):
+def createPermutation(permutation, remaining, currentTotalUtility, valuations, seatGraph, n):
     totalUtility = currentTotalUtility
     if len(remaining)==0:
-        return calculateUtility(permutation)
+        return calculateUtility(permutation, valuations, seatGraph, n)
     else:
         for i in remaining:
             permutation2 = permutation.copy()
             permutation2.append(i)
             remaining2 = remaining.copy()
             remaining2.remove(i)
-            utility = createPermutation(permutation2, remaining2, totalUtility)
+            utility = createPermutation(permutation2, remaining2, totalUtility, valuations, seatGraph, n)
             if utility > totalUtility:
                 totalUtility = utility
         return totalUtility
 
-permutation = []
-totalUtility = createPermutation(permutation, remaining, minUtility*maxDegree)
+def bruteForce(valFile, seatFile):
+    valuations = readFile(valFile)
+    seatGraph = readFile(seatFile)
 
-print("Totaltime: " + str((time.time() - start_total_time)) + " s")
-print('Utility = ', totalUtility)
+    n = len(valuations)
+    minValuation = getMaxMinValuations(valuations, n)
+    maxDegree = getMaxDegree(seatGraph, n)
+    minUtility = minValuation[0]*maxDegree
+    start_total_time = time.time()
+
+    remaining = []
+    for i in range(n):
+        remaining.append(i)
+
+    permutation = []
+    totalUtility = createPermutation(permutation, remaining, minUtility*maxDegree, valuations, seatGraph, n)
+    start_total_time = time.time() - start_total_time
+    output = {}
+    output['Total time'] = start_total_time
+    output['Build time'] = 0
+    output['Solve time'] = start_total_time
+    output['Total utility'] = totalUtility
+    return output
+
+if __name__ == '__main__':
+    valFile = sys.argv[1]
+    seatFile = sys.argv[2]
+    output = bruteForce(valFile, seatFile)
+    print(output)
