8aeeafd3677191a06f6f663ab8f0823c61d23bc8
José Rodríguez <2300456r@student.gla.ac.uk>
2025-01-17 17:20:40 +0000

    Overhaul file so that it is easier to both analyse more file and consider situations in which a model was executed only on some instances

--- seatArrangementAnalysis.py
+++ seatArrangementAnalysis.py
@@ -1,38 +1,73 @@
 import re
 import numpy as np
+from os import listdir
 
 def is_number(s):
     pattern = re.compile(r'^-?\d+(\.\d+)?$')
     return bool(pattern.match(s))
 
+directories = []
+objective = ''
+utility = ''
+interest = ''
+outputFile = ''
+
 files = []
 sizes = []
 data = {}
-outputFile = ''
-
+output = {}
 models = []
+
+for directory in directories:
+    newFiles = listdir(directory)
+    for dataFile in newFiles:
+        if objective in dataFile and utility in dataFile and interest in dataFile:
+            files.append(directory+'/'+dataFile)
+            size = int(dataFile.split('_')[1])
+            if size not in sizes:
+                sizes.append(size)
+sizes.sort()
 
 for i in range(len(files)):
     with open(files[i], 'r')as f:
         lines = f.read().splitlines()
         if is_number(lines[0]) == False:
-            models = lines[0].split(',')
-            lines = lines[1:]
+            newModels = lines[0].split(',')
+            for model in newModels:
+                if model not in models:
+                    models.append(model)
         else:
             models.append('data')
-        fileData = np.zeros((len(lines), len(lines[0].split(','))))
+models.sort()
+
+for model in models:
+    data[model] = {}
+    output[model] = {}
+    for size in sizes:
+        data[model][size] = []
+        output[model][size] = ''
+
+for i in range(len(files)):
+    with open(files[i], 'r')as f:
+        size = int(files[i].split('_')[1])
+        lines = f.read().splitlines()
+        newModels = []
+        if is_number(lines[0]) == False:
+            newModels = lines[0].split(',')
+            lines = lines[1:]
+        else:
+            newModels = ['data']
         for j in range(len(lines)):
             splitLine = lines[j].split(',')
             splitList = []
             for k in range(len(splitLine)):
-                fileData[j,k] = float(splitLine[k])
-        data[sizes[i]] = fileData
+                data[newModels[k]][size].append(float(splitLine[k]))
 
-output = np.zeros((len(sizes), len(models)))
-
-for i in range(len(sizes)):
-    for j in range(len(models)):
-        output[i,j]  = np.average(data[sizes[i]][:,j])
+for model in models:
+    for size in sizes:
+        if len(data[model][size]) > 0:
+            average = sum(data[model][size])/len(data[model][size])
+            output[model][size] = str(round(average, 2))
 
 with open(outputFile, 'w')as f:
     header = 'sizes,'+','.join(models)+'\n'
@@ -40,7 +75,7 @@ with open(outputFile, 'w')as f:
     for i in range(len(sizes)):
         line = str(sizes[i])+','
         for j in range(len(models)):
-            line += str(output[i,j])
+            line += output[models[j]][sizes[i]]
             if j < len(models) - 1:
                 line += ','
         line += '\n'
